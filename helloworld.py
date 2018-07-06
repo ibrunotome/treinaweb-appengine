e9a3b45652433634f1ed7748d41c4397726035f8
Bruno Tomé <ibrunotome@gmail.com>
2018-07-06 09:56:04 -0300

    Fix typo on helloworld.py and app.yml

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,10 @@
 import webapp2
 
-
-class HeloWorld(webapp2.Requesthandler)
+class HeloWorld(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello world!')
 
-app = webapp2.WSCGIApplication([
+app = webapp2.WSGIApplication([
     ('/', HeloWorld)
 ], debug=True)
